2e6ced13adf2f2064ab5911dcae5a7b18554ed87
corgofil <kuskova.2004@mail.ru>
2024-10-10 19:59:44 +0300

    question info

---
+++ src/pages/QuestionInfo.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {NavLink} from "react-router-dom";
+
+const QuestionInfo = () => {
+    return ( 
+        <div>
+            
+        </div>
+    );
+}
+ 
+export default QuestionInfo;
